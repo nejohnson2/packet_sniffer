413f642de0560e616def481420b0670b76a6f29a
Nicholas Johnson <nejohnson2@gmail.com>
2015-12-15 14:38:15 +0000

    init commit

---
+++ packets_sniffer.py
@@ -0,0 +1,41 @@
+from scapy.all import *
+import couchdb
+import time
+
+def db_setup():
+    # If your CouchDB server is running elsewhere, set it up like this:
+    couch = couchdb.Server('http://172.22.72.28:5984/')
+    # select database
+    db = couch['test']
+    return db
+    
+db = db_setup()
+
+def PacketHandler(db):
+	def send_to_db(pkt):
+		if pkt.haslayer(RadioTap) and pkt.haslayer(Dot11):        
+			if pkt.type == 0 and pkt.subtype == 4:
+				try:
+					ssid = pkt[Dot11ProbeReq].info
+				except:
+					ssid = " "
+				
+				print "%s : %s" %(pkt.addr2,ssid)
+				doc = {'type': 'request', 'addr2': pkt.addr2, 'ssid': ssid, 'time': time.time()}
+				db.save(doc)
+				# if pkt.addr2 not in device_list:
+				# 	device_list.append(pkt.addr2)
+				# 	doc = {'type': 'request', 'addr2': pkt.addr2, 'ssid': ssid, 'time': time.time()}
+				# 	db.save(doc)
+			
+# 			elif pkt.type == 0 and pkt.subtype == 5:
+# 				ssid = pkt[Dot11Elt][0].info
+# 				doc = {'type' : 'response', 'addr1': pkt.addr1, 'addr2': pkt.addr2, 'ssid': ssid, 'time': time.time()}
+# 				db.save(doc)
+			
+	return send_to_db
+
+sniff(iface="mon0", prn=PacketHandler(db))
+
+	
+
